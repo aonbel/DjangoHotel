dec3865bcd2dc7f15f689c64b8cbb985ef55976b
aonbel <anbel13219@gmail.com>
2025-05-20 17:07:03 +0300

    fix

--- HotelApp/hotel/settings.py
+++ HotelApp/hotel/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'hotel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['HotelApp/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
